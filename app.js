719d6ab25cccc331e9f16449a60050a64b4a7df6
Tchase44 <tchase4544@gmail.com>
2016-10-07 19:41:01 -0500

    humble beginnings
    simple styling is up, and simple functionallity is working!

---
+++ app.js
@@ -0,0 +1,85 @@
+var questions = [{
+	questionText : "How many 'types' of beer are there?",
+	answerOptions : ['2','10','30','50+'],
+	answerText : "all beers boil down to ales or lagers",
+	correctAnswer: 0
+} , {
+	questionText : "What does IPA stand for?",
+ 	answerOptions : ['Imperial Pale Ale','India Pale Ale','Inclusive Pale Ale','Imported Pale Ale'],
+	answerText : "Named from beer that shipped to India loaded with hops to keep from spoiling during the trip",
+	correctAnswer:1
+} , {
+	questionText : "What is the earlist estimated year beer is thought to have been made?",
+	answerOptions : ['9500 BC','7000 BC','4000 BC','2500 BC'],
+ 	answerText : "it is suspected humans first fermented grains around this time. 7,500-5,000 BC is the oldest confrimed fermented beverage",
+	correctAnswer: 0
+} , {
+	questionText : "What is the scientific name for the study of beer?",
+	answerOptions : ['Oenology','Rheology','Zythology','Zymology'],
+	answerText : "Zythology is the study of beer! Rheology,Oenology,Zymology are the studies of Liquids, Wine, and Fermentation respectivly.",
+	correctAnswer: 2
+} , {
+	questionText : "Which country consumes the most beer per person?",
+	answerOptions : ['Ireland','Germany','Czech Republic','United States'],
+	answerText : "The Czech Republic as a country drinks the most beer per person than anyother in the world",
+	correctAnswer: 2
+}];
+
+var questionCount = 0;
+
+
+$(document).ready(function(){
+
+	$('button#sumbitButton').hide();
+	$('form#js-form').hide();
+
+	$('button#startButton').click(function(event){
+		event.preventDefault();
+		// hide button show button
+		$('button#startButton').remove();
+		$('button#sumbitButton').show();
+		$('form#js-form').show();
+		// show 1st qestion
+		display();
+	});
+
+	$('button#sumbitButton').click(function(event){
+		event.preventDefault();
+		display();
+		checkAnswer();
+	});
+	// display();
+});
+
+// function myFunction() {
+// 	$( "#answerText" ).html( $( "input:checked" ).val() + " is checked!" );
+// };
+function checkAnswer(){
+	var a = $( "input:checked" ).val();
+	if (a == questions[questionCount].correctAnswer) {
+		$( "#answerText" ).text(questions[questionCount].answerText);
+		questionCount++;
+	} else {
+		$('#answerText').text("Wrong!");
+	}
+};
+
+function display(){
+	$("form#js-form p").text(questions[questionCount].questionText);
+	$("span.option1").text(questions[questionCount].answerOptions[0]);
+	$("span.option2").text(questions[questionCount].answerOptions[1]);
+	$("span.option3").text(questions[questionCount].answerOptions[2]);
+	$("span.option4").text(questions[questionCount].answerOptions[3]);
+	// questionCount++;
+};
+
+
+var template ='<form id="js-form">'+
+				'<p id="question">'+ '</p>'+
+				'<input type="radio" name="answer" class="option1 yay" id="ans" value="yup" />'+"<span class='option1'>"+ '</span>' +'<br>'+
+				'<input type="radio" name="answer" class="option2 yay" id="ans" value="nope" />'+"<span class='option1'>"+ '</span>' +'<br>'+
+				'<input type="radio" name="answer" class="option3 yay" id="ans" value="nope" />'+"<span class='option1'>"+ '</span>' +'<br>'+
+				'<input type="radio" name="answer" class="option4 yay" id="ans" value="nope" />'+"<span class='option1'>"+ '</span>' +'<br>'+
+				'<button type="sumbit" id="sumbitButton" class="button" name="submit" value="sumbit">Submit!</button>'+
+			'</form>'+
+			'<p id="answerText">'+ '</p>';
